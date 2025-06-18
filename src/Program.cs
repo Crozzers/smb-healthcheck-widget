fa83b076f60573b21c7641b3f5662bfdfcdd89e7
Crozzers <captaincrozzers@gmail.com>
2025-06-18 19:00:11 +0100

    Quicker connection check and better diagnosis of errors

--- src/Program.cs
+++ src/Program.cs
@@ -5,39 +5,38 @@ public struct SMBShare
 {
     public SMBShare(string address, string share)
     {
-        this.address = address;
-        this.share = share;
+        Address = address;
+        Share = share;
     }
-    public string address { get; init; }
-    public string share { get; init; }
+    public string Address { get; init; }
+    public string Share { get; init; }
+}
+
+public enum SMBDiagnosis
+{
+    Unreachable,
+    GuestLoginFailed,
+    ShareNotFound,
+    Unknown
 }
 
 class Program
 {
     static int Main()
     {
-        foreach (SMBShare share in ParseFstab())
+        var shares = ParseFstab();
+        for (var i = 0; i < shares.Count(); i++)
         {
-            var client = new SMB2Client();
-            bool connected = client.Connect(share.address, SMBTransportType.DirectTCPTransport);
+            var share = shares[i];
 
-            Console.WriteLine($"{share.address}: {connected}");
-
-            NTStatus status = client.Login(String.Empty, "GUEST", String.Empty);
-            if (status == NTStatus.STATUS_SUCCESS)
+            if (IsShareMounted(share))
             {
-                var shares = client.ListShares(out status);
-                foreach (var smallShare in shares)
-                {
-                    Console.WriteLine($"Share: {smallShare}");
-                }
+                Console.WriteLine($"{share.Address}/{share.Share}: ok");
             }
             else
             {
-                Console.WriteLine("Guest login failed");
+                Console.WriteLine($"{share.Address}/{share.Share}: {DiagnoseSMBConnection(share)}");
             }
-
-            client.Disconnect();
         }
         return 0;
     }
@@ -57,12 +56,59 @@ class Program
                 continue;
             }
 
+            if (line.Split([' ', '\t'])[2] != "cifs")
+            {
+                continue;
+            }
+
             drive = drive.Replace("//", "");
             var address = drive.Split('/')[0];
 
-            shares.Add(new SMBShare(address, drive.Replace(address, "")));
+            shares.Add(new SMBShare(address, drive.Replace(address, "").Trim('/')));
         }
 
         return shares;
+    }
+
+    static bool IsShareMounted(SMBShare share)
+    {
+        var reader = new StreamReader("/proc/mounts");
+        string? line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            if (line.StartsWith($"//{share.Address}/{share.Share}"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    static SMBDiagnosis DiagnoseSMBConnection(SMBShare share)
+    {
+        var client = new SMB2Client();
+        bool connected = client.Connect(share.Address, SMBTransportType.DirectTCPTransport);
+
+        if (!connected)
+        {
+            return SMBDiagnosis.Unreachable;
+        }
+
+        NTStatus status = client.Login(String.Empty, "GUEST", String.Empty);
+        if (status != NTStatus.STATUS_SUCCESS)
+        {
+            return SMBDiagnosis.GuestLoginFailed;
+        }
+
+        var shareAvailable = client.ListShares(out status).Contains(share.Share);
+        client.Logoff();
+        client.Disconnect();
+
+        if (!shareAvailable)
+        {
+            return SMBDiagnosis.ShareNotFound;
+        }
+
+        return SMBDiagnosis.Unknown;
     }
 }
